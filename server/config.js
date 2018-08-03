0725c630f73d06d8324ccef5f702bcf492cc0e12
luwis lopez <spartanhero93@gmail.com>
2018-08-03 17:21:17 -0400

    first stage of fetch completed

--- server/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  API_KEY: 'cm6gpefe96s93z82ay2aqykv9f4zjdwh'
-}
--- server/server.js
+++ server/server.js
@@ -2,20 +2,25 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const axios = require('axios')
-const API_KEY = require('./config')
+const keys = require('./config/keys')
 
 app.get('/getData', async (req, res) => {
   try {
     const response = await axios.get(
-      `https://us.api.battle.net/wow/auction/data/darkspear?locale=en_US&apikey=${API_KEY}`
+      `https://us.api.battle.net/wow/auction/data/darkspear?locale=en_US&apikey=${keys.API_KEY}`
     )
 
-    console.log(response.data)
+    const data = await response.data.files,
+      ApiUrl = await data[0].url,
+      lastModified = await data[0].lastModified
+
+    console.log(lastModified)
+
     // const realm = response.data.realms
     // const auctions = response.data.auctions
     res.send({ completed: true })
   } catch (e) {
-    console.log(e)
+    res.send({ completed: false })
   }
 })
 
